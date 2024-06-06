1bd31d206f4ce18fb19d8786ce6201c5f3f90135
wilson40073 <2023-40073@liceocanadiense.edu.gt>
2024-06-06 14:12:50 -0600

    se van a subir 3 proyectos

---
+++ ejemplo1.py
@@ -0,0 +1,15 @@
+a=5
+b=8
+c=9
+d=2
+e=1
+d=a+b+c
+f=d+e
+a1=a*b*c*d*e
+a2=a+b+c+d+e
+a3=f-d
+a4=f/d
+print("la multiplicación de los 5 numeros ingresados es: ", a1)
+print("la suma de los 5 numeros ingresados es: ", a2)
+print("la resta de los 5 numeros ingresados es: ", a3)
+print("la división de los 5 numeros ingresados es: ", a4)
