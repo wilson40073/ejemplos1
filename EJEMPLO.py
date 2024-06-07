de13d31d4ee4674fe3a6b8993d868d74c6cbc1ec
wilson40073 <2023-40073@liceocanadiense.edu.gt>
2024-06-07 12:41:38 -0600

    prácticas del ejercicio 1 y 2

---
+++ EJEMPLO.py
@@ -0,0 +1,4 @@
+n1= int (input("ingrese su primer dato \n"))
+n2= int (input("ingrese su segundo dato \n"))
+resultado=n1+n2
+print("la suma de los datos es: " + str(resultado))
---
+++ practica2.py
@@ -0,0 +1,15 @@
+a= int (input("ingrese su primer dato \n"))
+b= int (input("ingrese su segundo dato \n"))
+c= int (input("ingrese su tercero dato \n"))
+d= int (input("ingrese su cuarto dato \n"))
+e= int (input("ingrese su quinto dato \n"))
+
+d=a+b+c+d
+a1=a*b*c*d*e
+a2=a+b+c+d+e
+a3=d-e
+a4=d/e
+print("la multiplicación de los 5 numeros ingresados es: ", a1)
+print("la suma de los 5 numeros ingresados es: ", a2)
+print("la resta de los 5 numeros ingresados es: ", a3)
+print("la división de los 5 numeros ingresados es: ", a4)
